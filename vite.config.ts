88d9e3f6c9c6da2ec1b4ef59f0b3a6b86371c6df
Santiago Cocco <santiago.cocco95@gmail.com>
2026-04-09 18:26:52 +0200

    chore: add @types/node as a devDependency and update vite.config for build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from "path";
 
 export default defineConfig({
   base: '/Conquerblocks_css_entregable_ModernArtGallery/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        location: resolve(__dirname, "location.html"),
+      },
+    },
+  },
 });
